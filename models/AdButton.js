e7cb0e82af47ae6b8f82557fa534e21acab88bb1
shuji-chu <chulingli1988@outlook.com>
2026-08-20 12:21:31 +0800

    Create AdButton model with schema definition

---
+++ models/AdButton.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const adButtonSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    url: { type: String, default: '' },
+    order: { type: Number, default: 0 }
+});
+module.exports = mongoose.model('AdButton', adButtonSchema);
