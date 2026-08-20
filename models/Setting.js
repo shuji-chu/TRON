14a56bc7cc589bdc8dec4c81d0796364c6f00c50
shuji-chu <chulingli1988@outlook.com>
2026-08-20 12:55:52 +0800

    更新 Setting.js

--- models/Setting.js
+++ models/Setting.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+
 const settingSchema = new mongoose.Schema({
     usdtReceiveAddress: { type: String, default: '' },
     qrCodeFileId: { type: String, default: '' },
     createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Setting', settingSchema);
